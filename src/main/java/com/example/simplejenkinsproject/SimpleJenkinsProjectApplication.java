d88e8c18a107cf6d724cbbccb1cdee72d561b0cf
4everTheOne <j.a.h.oliveira@hotmail.com>
2021-12-01 15:31:41 +0000

    First commit

---
+++ src/main/java/com/example/simplejenkinsproject/SimpleJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplejenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJenkinsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJenkinsProjectApplication.class, args);
+    }
+
+}
